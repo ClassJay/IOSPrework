beaa4a44882d4d2a9c82cace8611c3375f8135b0
Julianah Adubi <69608172+ClassJay@users.noreply.github.com>
2021-12-16 17:02:30 -0500

    Submitting PreWork

---
+++ SettingsViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SettingsViewController.swift
+//  CodePath Prework
+//
+//  Created by Julianah Adubi on 11/28/21.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func ToggleDarkMode(_ sender: UISwitch) {
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
